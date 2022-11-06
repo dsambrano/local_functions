91dcf1f3291ff0c81012455532e2c330f60f8357
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-11-05 20:06:13 -0700

    Pulling out Code from Main to use as individual functions

--- python/change_alacritty_theme.py
+++ python/change_alacritty_theme.py
@@ -8,6 +8,33 @@ ALACRITTY_CONFIG = Path.home() / Path(".config/alacritty/alacritty.yml")
 THEMES_DIR = ALACRITTY_CONFIG.parent / Path("themes/")
 THEME_TEXT = "~/.config/alacritty/themes/"  # Unique Pattern Inside alacritty config
 
+
+def random_theme() -> str:
+    """TODO: Docstring for random_theme.
+    Returns: TODO
+
+    """
+    themes = [themes.stem for themes in THEMES_DIR.iterdir()]
+    theme = themes[random.randint(0, len(themes) - 1)]
+    return theme
+
+
+def regex_config(config: Path, new_pattern: str, old_pattern: str, **kwargs) -> None:
+    """TODO: Docstring for regex_config.
+
+    Updates the Config File based on Regex Pattern provided.
+
+    Returns: None, but edits file destructively.
+
+    """
+    with config.open("r") as f:
+        content = f.read()
+        new_content = re.sub(old_pattern, new_pattern, content, **kwargs)
+
+    with config.open("w") as f:
+        f.write(new_content)
+
+
 def main() -> None:
     """Main
 
@@ -26,6 +53,7 @@ def main() -> None:
 
     with ALACRITTY_CONFIG.open("w") as f:
         f.write(new_content)
+
 
 if __name__ == "__main__":
     main()
