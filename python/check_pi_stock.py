da72efb03f0dac013b0f266e3909ade366e0b4c8
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-11-09 15:04:08 -0800

    Check Raspberry Pi Stock

---
+++ python/check_pi_stock.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+BASE_URL = "https://www.microcenter.com/product/"
+products = {
+    "pi zero": "643085/pizero2w",
+    "4 Channel Relay Sheild": "643966/inland-rpi-4-channel-relay-5v-shield-for-raspberry-pi-ce-certification",
+    "pi 3": "460968/raspberry-pi-3-model-b",
+    "pi 4 2gb": "621439/raspberry-pi-4-model-b-2gb-ddr4",
+    "pico H": "650107/raspberry-pi-pico-h-raspberry-pi-pico-with-headers-pre-installed",
+    "pico W": "650108/raspberry-pi-pico-w",
+}
+adafruit_url = "https://www.adafruit.com/product/"
+adafruit = {
+    "pi 4 2GB": "4292",
+    "pi 4 1GB": "4295",
+    "pi 4 4GB": "4296",
+    "Zero": "3400",
+    "Zero WH": "3708",
+    "Zero 2W": "5291",
+    "pico W": "5526",
+    "pico H": "5525",
+    "pico WH": "5544",
+}
+pishop_url = "https://www.pishop.us/product/"
+pihsip = {
+    "pi 4 1GB": "raspberry-pi-4-model-b-1gb/",
+    "pi 4 2GB": "raspberry-pi-4-model-b-2gb/",
+    "pi 4 4GB": "raspberry-pi-4-model-b-4gb/",
+}
+chicago_url = "https://chicagodist.com/products/"
+chicago = {
+    "pi 4 1GB": "craspberry-pi-4-model-b-1gb",
+    "pi 4 2GB": "craspberry-pi-4-model-b-2gb",
+    "pi 4 4GB": "craspberry-pi-4-model-b-4gb",
+}
+cana_url = "https://www.canakit.com/"
+cana = {
+        "pi 4 1GB":   "raspberry-pi-4.html",
+        "pi 4 2GB":   "raspberry-pi-4-2gb.html",
+        "pi 4 4GB":   "raspberry-pi-4-4gb.html",
+        }
