8bd165a45182b6cee13ca7c930c059d1e31979e0
Deshawn Sambrano <dsambrano@users.noreply.github.com>
2022-12-12 22:06:41 -0800

    Fixing Typo in run.py call for change_theme

--- shell/change_theme.sh
+++ shell/change_theme.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/home/buddy/git_repos/local_functions/python/super_rice/venv/bin/python3 ~/git_repos/local_functions/python/super_rice/run.y
+~/git_repos/local_functions/python/super_rice/venv/bin/python3 ~/git_repos/local_functions/python/super_rice/run.py
